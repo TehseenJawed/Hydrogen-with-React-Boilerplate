b160177eb0adc2242653b482b329e033a05adfad
imrandipixels <imrandipixels@gmail.com>
2022-02-18 06:07:38 +0500

    Hydrogen boilerplate

---
+++ shopify.config.js
@@ -0,0 +1,7 @@
+export default {
+  defaultLocale: 'en-us',
+  storeDomain: 'tesingapps.myshopify.com',
+  storefrontToken: '217e76bbb02a7d3bbf93ac8d04afde10',
+  // storefrontToken: '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',
+  storefrontApiVersion: 'unstable',
+};
